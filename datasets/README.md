7a1f39c9e23396642765b6036ec00a9d454fa5ab
柏灌 <baiguan.yt@alibaba-inc.com>
2022-04-15 17:58:48 +0800

    Update README

--- datasets/README.md
+++ datasets/README.md
@@ -2,3 +2,60 @@
 
 This folder is mainly for storing datasets used for training/validation/testing.
 
+### VFI
+
+Make data structure to be:
+```
+├────vimeo_triplet
+	├────train
+		├────00001_0001
+			├────im1.png
+			├────im2.png
+			├────im3.png
+		├────00001_0002
+			├────im1.png
+			├────im2.png
+			├────im3.png
+		...
+
+	├────validation
+		├────00001_0389
+			├────im1.png
+			├────im2.png
+			├────im3.png
+		...
+```
+- Training
+
+	[Adobe240fps](http://www.cs.ubc.ca/labs/imager/tr/2017/DeepVideoDeblurring/) | [vimeo triplet](http://toflow.csail.mit.edu/)
+
+- Testing
+
+	[UCF101 triplet](https://github.com/liuziwei7/voxel-flow) | [Middleburry](https://vision.middlebury.edu/flow/data/)
+
+Please following this [instruction](https://github.com/avinashpaliwal/Super-SloMo/tree/master/data) to prepare Adobe240fps training/validation sets.
+
+### Morphing
+
+- Training
+
+	dog2dog | cat2cat
+
+### I2I
+
+- Training
+
+	Face aging | Face toonification
+
+
+### Style Transfer
+
+- Training
+
+	[coco](https://cocodataset.org/) | [wikiart](https://github.com/cs-chan/ArtGAN/tree/master/WikiArt%20Dataset)
+
+### Super Resolution
+
+- Training
+ 	
+ 	[DIV2K](https://data.vision.ee.ethz.ch/cvl/DIV2K/)
--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ python sdl_test.py --task vfi-dir --model SDL_vfi_perceptual.pth --num 1 --indir
 
 #python sdl_test.py --task morphing --model SDL_cat2cat_scale.pth --num 7 --size 512 --source examples/Morphing/cat/source/flickr_cat_000008.jpg --target examples/Morphing/cat/target/pixabay_cat_000010.jpg --outdir results/Morphing/cat
 
-#python sdl_test.py --task i2i --model SDL_aging_scale.pth --num 7 --size 512 --extend_t --source examples/I2I/ffhq-10/00002.png --outdir results/I2I/aging
+#python sdl_test.py --task i2i --model SDL_aging_scale.pth --in_ch 3 --num 7 --size 512 --extend_t --source examples/I2I/ffhq-10/00002.png --outdir results/I2I/aging
 
-#python sdl_test.py --task i2i --model SDL_toonification_scale.pth --num 7 --size 512 --source examples/I2I/ffhq-10/00002.png --outdir results/I2I/toonification
+#python sdl_test.py --task i2i --model SDL_toonification_scale.pth --in_ch 3 --num 7 --size 512 --source examples/I2I/ffhq-10/00002.png --outdir results/I2I/toonification
 
 #python sdl_test.py --task style_transfer --model SDL_style_transfer_arbitrary.pth --num 7 --source examples/Style_transfer/content/sailboat.jpg --target examples/Style_transfer/style/sketch.png --outdir results/Style_transfer
 
